ca6d4eb13b451d5c0462bc38b4e56820b91669a5
DmmDGM <dm12332131mdgaming@gmail.com>
2025-10-25 14:19:01 -0400

    imported sources to index

--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,18 @@
 import { Hono } from "hono";
 import * as source from "./source";
 
-// console.log(await source.fetchMapVehiclePoints());
-// console.log(await source.fetchVehicleCapacities());
-// console.log(await source.fetchArrivalTimes());
-console.log(await source.fetchRouteMaps());
+// Defines sources
+let mapVehiclePoints = await source.fetchMapVehiclePoints();
+let vehicleCapacities = await source.fetchVehicleCapacities();
+let arrivalTimes = await source.fetchArrivalTimes();
+let routeMaps = await source.fetchRouteMaps();
+setInterval(async () => {
+    mapVehiclePoints = await source.fetchMapVehiclePoints();
+    vehicleCapacities = await source.fetchVehicleCapacities();
+    arrivalTimes = await source.fetchArrivalTimes();
+    routeMaps = await source.fetchRouteMaps();
+}, 5 * 60 * 1000);
+
 // Defines app
 const app = new Hono()
     .get("/hello", (context) => {
